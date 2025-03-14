1cb336310f645b0e0f2969513291f42d6632e0c9
lucasnunessg <lucasnunespacheco@gmail.com>
2025-03-14 17:03:38 -0300

    first commit

---
+++ src/main/java/map_weather/project/controller/NominatiumController.java
@@ -0,0 +1,37 @@
+package map_weather.project.controller;
+
+import map_weather.project.service.NominatiumService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/rotas")
+public class NominatiumController {
+
+  private final NominatiumService nominatiumService;
+
+  public NominatiumController(NominatiumService nominatiumService) {
+    this.nominatiumService = nominatiumService;
+  }
+
+  @GetMapping("/geo")
+  public Mono<String> geoCode(@RequestParam String endereco) {
+    return nominatiumService.buscarCoordenadas(endereco);
+  }
+
+  @GetMapping("/travel")
+  public Mono<String> buscarCity(@RequestParam String origem, @RequestParam String destino){
+    return nominatiumService.buscar2Coordenadas(origem, destino)
+        .onErrorResume(e -> Mono.just("Erro ao buscar coordenadas: " + e.getMessage()));
+  }
+
+  @GetMapping("/travels")
+  public Mono<String> buscarCityWithStop(@RequestParam String origem, @RequestParam String parada, @RequestParam String destino){
+    return nominatiumService.buscar3Coordenadas(origem, parada, destino)
+        .onErrorResume(e -> Mono.just("Erro ao buscar coordenadas: " + e.getMessage()));
+  }
+
+}
