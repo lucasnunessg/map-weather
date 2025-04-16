a1db7dbb4d4ef2c0c39f589b5343630e5fe4073e
lucasnunessg <lucasnunespacheco@gmail.com>
2025-04-15 21:14:19 -0300

    ja começando o front

---
+++ routes-weather/src/components/Weather.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export function Weather(){
+
+
+
+const [weather, setWeather] = useState(null);
+const [city, setCity] = useState("");
+
+  return(
+    <></>
+  )
+}
