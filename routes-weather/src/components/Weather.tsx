037ccf35375483ea8cbbca8f30d218e162abf2a9
lucasnunessg <lucasnunespacheco@gmail.com>
2025-04-23 20:18:55 -0300

    retornando

--- routes-weather/src/components/Weather.tsx
+++ routes-weather/src/components/Weather.tsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import api from "./Fetch";
 
-export function Weather(){
+export async function Weather(){
 
 
 
 const [weather, setWeather] = useState(null);
 const [city, setCity] = useState("");
+
+
+  const fetchData = async () => {
+    try {
+      
+      const response = await api.get("previsao-tempo/previsao-cidade", {
+        city
+      })
+
+    }catch(e){
+      console.error(e)
+    }
+  }
 
   return(
     <></>
